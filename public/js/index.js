94a34bc169a149d09497d370c6b28960fd04e4c7
dallinn <dallinn@users.noreply.github.com>
2017-02-07 22:46:48 -0700

    collect all options and suboptions in basket

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,9 @@
 var types = getTypes();
 var select = document.getElementById('type');
 var div = document.getElementById('subtypes');
+var table = document.getElementById('table');
 
-select.onchange = function() {
+select.onchange = function(){
     showSubOptions();
 }
 // ajax to /types uri, sets all option tags from api
@@ -34,6 +35,7 @@ function showSubOptions(){
     while(div.hasChildNodes()){
         div.removeChild(div.lastChild);
     }
+
     var selectValue = (select.value).split(',')[1]; 
     var suboptions = types[selectValue].suboptions;
     suboptions.forEach(function(s){
@@ -41,6 +43,7 @@ function showSubOptions(){
         check.type = 'checkbox';
         check.id = s;
         check.name = 'so';
+        //TODO: format values
         check.value = s;
 
         var label = document.createElement('label');
@@ -51,9 +54,69 @@ function showSubOptions(){
         div.appendChild(label);
         div.appendChild(document.createElement('br'));
     });
+
+    var addToB = document.createElement('input');
+    addToB.type = 'submit';
+    addToB.value = 'Add to Table';
+    div.appendChild(addToB);
+
 }
+
 var form = document.getElementById('basketAdder');
+form.onsubmit = function(){
+    event.preventDefault();
+    addDataToBasket();
+}
+
 
 function addDataToBasket(){
-    
+    var type = document.getElementById('type');
+        type = type.options[type.selectedIndex].value.split(',')[0];
+    var checks = document.getElementsByName('so');
+    var subtypes = [];
+    for (var i=0;i<checks.length;i++){
+        if (checks[i].checked){
+            subtypes.push(checks[i].value);
+        }
+    }
+    //if (subtypes) subtypes = subtypes.substring(1);
+    var slist = document.createElement('div');
+    table.appendChild(slist);
+    var head = document.createElement('h4');
+    var h = document.createTextNode(type);
+    head.appendChild(h);
+    slist.appendChild(head);
+
+    subtypes.forEach(function(s){
+        var item = document.createElement('li');
+        var iv = document.createTextNode(s); 
+        item.appendChild(iv);
+        slist.appendChild(item);
+    });
+
+    var genForm = document.getElementById('generate');
+    genForm.style.display = 'block';
+    genForm.onsubmit = function(){
+        var all = table.children;
+
+        for (var i = 0; i<all.length; i++){
+            for (var j = 0; j<all[i].children.length; j++){
+                var hidden = document.createElement('input');
+                hidden.type = 'hidden';
+                if (all[i].children[j].tagName == 'H4'){
+                    hidden.name = 'option';
+                    hidden.value = all[i].children[j].innerHTML;
+                    console.log('OPTION: ' + all[i].children[j].innerHTML);
+                } else {
+                    hidden.name = 'so';
+                    hidden.value = all[i].children[j].innerHTML;
+                    console.log('SUBOPTIONS:' + all[i].children[j].innerHTML);
+                }
+            genForm.appendChild(hidden);
+            }
+        }
+        console.log(genForm.children);
+        //event.preventDefault();
+
+    }
 }
